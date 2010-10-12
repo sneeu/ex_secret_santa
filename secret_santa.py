8f1089b67bb6c2c3b6e7b66b7f900015978c8b42
sneeu <john@sneeu.com>
2010-10-12 16:51:28 +0100

    Fixes a bug where it would leave one person off.

--- secret_santa.py
+++ secret_santa.py
@@ -16,13 +16,19 @@ def main():
     pairs = list(PAIRS)
     random.shuffle(pairs)
 
-    for i in xrange(len(pairs) - 1):
-        print '%s -> %s' % (pairs[i][0], pairs[i + 1][1], )
+    for i in xrange(len(pairs)):
+        buys_for = i - 1
+        if i == 0:
+            next = len(pairs)
+        print '%s -> %s' % (pairs[i][0], pairs[buys_for][1], )
 
     random.shuffle(pairs)
 
-    for i in xrange(len(pairs) - 1):
-        print '%s -> %s' % (pairs[i][1], pairs[i + 1][0], )
+    for i in xrange(len(pairs)):
+        buys_for = i - 1
+        if i == 0:
+            next = len(pairs)
+        print '%s -> %s' % (pairs[i][1], pairs[buys_for][0], )
 
 
 if __name__ == '__main__':
