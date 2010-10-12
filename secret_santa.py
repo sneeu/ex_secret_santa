f742b113ea90eb2ba26b188e5877d1ff070da45b
sneeu <john@sneeu.com>
2010-10-12 17:30:43 +0100

    Adds tests.

--- secret_santa.py
+++ secret_santa.py
@@ -1,4 +1,5 @@
 import random
+import unittest
 
 
 PAIRS = (
@@ -12,16 +13,52 @@ PAIRS = (
 )
 
 
-def main():
-    pairs = list(PAIRS)
+def generate_buyers(pairs):
+    buyers = []
 
     for i in xrange(len(pairs[0])):
         random.shuffle(pairs)
 
         for j in xrange(len(pairs)):
             buys_for = (j + 1) % len(pairs)
-            print '%s -> %s' % (pairs[j][i], pairs[buys_for][(i + 1) % len(pairs[buys_for])], )
+            buyers.append((
+                pairs[j][i],
+                pairs[buys_for][(i + 1) % len(pairs[buys_for])], ))
+
+    return buyers
+
+
+class TestGenerateBuyers(unittest.TestCase):
+    def test_generate_buyers(self):
+        import itertools
+
+        pairs = [
+            ('A', 'B', ),
+            ('C', 'D', ),
+            ('E', 'F', ),
+            ('G', 'H', ),
+        ]
+        result = generate_buyers(pairs)
+
+        assert (len(result) == 2 * len(pairs))
+        assert sorted([r[0] for r in result]) ==\
+            sorted([r[1] for r in result]) == sorted(itertools.chain(*pairs))
+
+
+def test():
+    unittest.main()
+
+
+def main():
+    print '\n'.join(['%s -> %s' % (f, t)
+        for (f, t) in generate_buyers(list(PAIRS))])
 
 
 if __name__ == '__main__':
-    main()
+    import sys
+
+    if len(sys.argv) > 1 and sys.argv[1] == '--test':
+        del sys.argv[1]
+        test()
+    else:
+        main()
