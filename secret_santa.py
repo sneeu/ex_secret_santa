a25510c7e713a178fd8c75def7f7994ad16bc5e6
sneeu <john@sneeu.com>
2010-10-12 16:54:55 +0100

    Removes duplication, and uses % instead of if.

--- secret_santa.py
+++ secret_santa.py
@@ -14,21 +14,13 @@ PAIRS = (
 
 def main():
     pairs = list(PAIRS)
-    random.shuffle(pairs)
 
-    for i in xrange(len(pairs)):
-        buys_for = i - 1
-        if i == 0:
-            next = len(pairs)
-        print '%s -> %s' % (pairs[i][0], pairs[buys_for][1], )
+    for i in xrange(len(pairs[0])):
+        random.shuffle(pairs)
 
-    random.shuffle(pairs)
-
-    for i in xrange(len(pairs)):
-        buys_for = i - 1
-        if i == 0:
-            next = len(pairs)
-        print '%s -> %s' % (pairs[i][1], pairs[buys_for][0], )
+        for j in xrange(len(pairs)):
+            buys_for = (j + 1) % len(pairs)
+            print '%s -> %s' % (pairs[j][i], pairs[buys_for][(i + 1) % len(pairs[buys_for])], )
 
 
 if __name__ == '__main__':
